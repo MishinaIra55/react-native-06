933463ac4c3c65f8726d871c4b20df58e2b28d54
MishynaIra <gortenzia1987@gmail.com>
2023-05-20 16:13:11 +0300

    Отображение локации на карте

--- Screens/nestedScreens/DefaultScreenPosts.js
+++ Screens/nestedScreens/DefaultScreenPosts.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, FlatList, Image, Button } from "react-native";
+import {View, StyleSheet, FlatList, Image, Text, TouchableOpacity} from "react-native";
 
 import {firestore} from '../../firebase/config';
 import { collection, onSnapshot } from "firebase/firestore";
+import {EvilIcons, MaterialCommunityIcons} from "@expo/vector-icons";
 
 export const DefaultScreenPosts = ({  navigation }) => {
     const [posts, setPosts] = useState([]);
@@ -26,29 +27,28 @@ export const DefaultScreenPosts = ({  navigation }) => {
     console.log("posts", posts);
     return (
         <View style={styles.container}>
-            <FlatList
-                data={posts}
-                keyExtractor={(item, indx) => indx.toString()}
-                renderItem={({ item }) => (
-                    <View
-                        style={{
-                            marginBottom: 10,
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Image
-                            source={{ uri: item.photo }}
-                            style={{ width: 350, height: 200 }}
-                        />
+            <FlatList data={posts} keyExtractor={(item, idx) => idx.toString()} renderItem={({item}) =>
+
+                <View style={{marginBottom: 32, width: '100%', height: 250}}>
+                    <Image style={{width: '100%', height: 200}} source={item.photo ? {uri: item.photo} : null}/>
+                    <View>
+                        <Text style={styles.name}>{item.name}</Text>
                     </View>
-                )}
-            />
-            <Button title="go to map" onPress={() => navigation.navigate("Map")} />
-            <Button
-                title="go to Comments"
-                onPress={() => navigation.navigate("Comments")}
-            />
+                    <View style={{flex: 1, flexDirection: "row", justifyContent: 'space-between'}}>
+                        <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', flexDirection: "row"}}  activeOpacity={0.8}  onPress={() => navigation.navigate('Comments', {postId: item.id, photo: item.photo})}>
+                            <Text>
+                                <EvilIcons  name="comment" size={24} color="#BDBDBD" />
+                            </Text>
+                            <Text style={{color: '#BDBDBD'}}>0</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', flexDirection: "row"}}  activeOpacity={0.8} onPress={() => navigation.navigate('Map', {location: item.location}) }>
+                            <Text>
+                                <MaterialCommunityIcons name="google-maps" size={24} color="#BDBDBD" />
+                            </Text>
+                            <Text style={{color: '#212121', textDecorationLine: 'underline'}}>{item.nameLocation}</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>}/>
         </View>
     );
 };
@@ -58,5 +58,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
     },
+    name: {
+        fontSize: 16,
+        lineHeight: 19,
+        marginBottom: 10,
+        color: '#212121',
+    }
 });
 
--- Screens/nestedScreens/MapScreen.js
+++ Screens/nestedScreens/MapScreen.js
@@ -2,16 +2,18 @@ import React from "react";
 import { View,  StyleSheet } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 
-export const MapScreen = () => {
+export const MapScreen = ({route}) => {
+    console.log("route.params.location", route.params.location);
+    const { latitude, longitude} = route.params.location.coords
     return (
         <View style={styles.container}>
             <MapView style={{flex: 1}} initialRegion={{
-                latitude: 45.6486,
-                longitude: 25.6061,
+                latitude,
+                longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
             }}>
-                <Marker coordinate={{latitude: 45.6486, longitude: 25.6061}}/>
+                <Marker coordinate={{latitude, longitude}}/>
             </MapView>
         </View>
     );
